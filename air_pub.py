2cbb9b94a549907d56afff87c0adc39219a28f72
kitar-kks <kroekkiat.kitar@gmail.com>
2022-02-04 14:34:16 +0700

    fix

--- air_pub.py
+++ air_pub.py
@@ -13,29 +13,30 @@ auth = {
 
 def talker():
     Rset_time = rospy.Publisher('set_time', Int8, queue_size=10)
-    # Rset_temp_on = rospy.Publisher('set_temp_on', Int8, queue_size=10)
-    # Rset_temp_off = rospy.Publisher('set_temp_off', Int8, queue_size=10)
-    # Rset_humid_on = rospy.Publisher('set_humid_on', Int8, queue_size=10)
-    # Rset_humid_off = rospy.Publisher('set_humid_off', Int8, queue_size=10)
+    Rset_temp_on = rospy.Publisher('set_temp_on', Int8, queue_size=10)
+    Rset_temp_off = rospy.Publisher('set_temp_off', Int8, queue_size=10)
+    Rset_humid_on = rospy.Publisher('set_humid_on', Int8, queue_size=10)
+    Rset_humid_off = rospy.Publisher('set_humid_off', Int8, queue_size=10)
 
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         msg = subscribe.simple("air_iot/set_time", hostname=hostname,auth=auth)
-        temp_on = subscribe.simple("air_iot/set_temp_on", hostname=hostname ,auth=auth)
-        temp_off = subscribe.simple("air_iot/set_temp_off", hostname=hostname ,auth=auth)
-        humid_on = subscribe.simple("air_iot/set_humid_on",hostname=hostname,auth=auth)
-        humid_off = subscribe.simple("air_iot/set_humid_on",hostname=hostname,auth=auth)
+        # temp_on = subscribe.simple("air_iot/set_temp_on", hostname=hostname ,auth=auth)
+        # temp_off = subscribe.simple("air_iot/set_temp_off", hostname=hostname ,auth=auth)
+        # humid_on = subscribe.simple("air_iot/set_humid_on",hostname=hostname,auth=auth)
+        # humid_off = subscribe.simple("air_iot/set_humid_on",hostname=hostname,auth=auth)
 
         time_for_arduino = int(msg.payload,10)
-        set_temp_on = int(temp_on.payload,10)
-        set_temp_off = int(temp_off.payload,10)
-        set_humid_on = int(humid_on.payload,10)
-        set_humid_off = int(humid_off.payload,10)
+        # set_temp_on = int(temp_on.payload,10)
+        # set_temp_off = int(temp_off.payload,10)
+        # set_humid_on = int(humid_on.payload,10)
+        # set_humid_off = int(humid_off.payload,10)
         
         rospy.loginfo(time_for_arduino)
         Rset_time.publish(time_for_arduino)
         # Rset_temp_on.publish(set_temp_on)
+        Rset_temp_on.publish(0)
         # Rset_temp_off.publish(set_temp_off)
         # Rset_humid_on.publish(set_humid_on)
         # Rset_humid_off.publish(set_humid_off)
